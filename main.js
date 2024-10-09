f5b466b6a47209ce93dd895f3bf344a74aeef360
MurotjonAbidjanov <murotzonabidzanov@gmail.com>
2024-10-09 09:49:46 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,97 @@
+// let stylez = ['Джаз', 'Блюз']
+// console.log(stylez)
+// stylez.push('Рок-н-Ролл')
+// console.log(stylez)
+// stylez[1] = 'Классика'
+// console.log(stylez)
+// stylez.shift()
+// console.log(stylez)
+// stylez.unshift('Реп', 'Регги')
+// console.log(stylez)
+// let arr1 = [`Ali`, `Asad`];
+// console.log(arr1);
+// arr1.push(`Salohiddin`)
+// console.log(arr1);
+// arr1.pop();
+// console.log(arr1);
+// arr1.unshift(`Umar`);
+// arr1.push(`Yusuf`);
+// console.log(arr1);
+// let arr1 = ["Asad", "Umar", "Ali", "Imron", "Yusuf"];
+// console.log(arr1);
+// arr1.push("Salohiddin");
+// console.log(arr1)
+// arr1.shift();
+// console.log(arr1);
+// arr1[2] = "Zain";
+// arr1.sort();
+// console.log(arr1)
+// let arr2 = arr1.slice(1, 4)
+// console.log(arr2)
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
+// let [a, b, c] = arr1
+// console.log(a, b)// let stylez = ['Джаз', 'Блюз']
+// console.log(stylez)
+// stylez.push('Рок-н-Ролл')
+// console.log(stylez)
+// stylez[1] = 'Классика'
+// console.log(stylez)
+// stylez.shift()
+// console.log(stylez)
+// stylez.unshift('Реп', 'Регги')
+// console.log(stylez)
+// let arr1 = [`Ali`, `Asad`];
+// console.log(arr1);
+// arr1.push(`Salohiddin`)
+// console.log(arr1);
+// arr1.pop();
+// console.log(arr1);
+// arr1.unshift(`Umar`);
+// arr1.push(`Yusuf`);
+// console.log(arr1);
+// let arr1 = ["Asad", "Umar", "Ali", "Imron", "Yusuf"];
+// console.log(arr1);
+// arr1.push("Salohiddin");
+// console.log(arr1)
+// arr1.shift();
+// console.log(arr1);
+// arr1[2] = "Zain";
+// arr1.sort();
+// console.log(arr1)
+// let arr2 = arr1.slice(1, 4)
+// console.log(arr2)
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
+// let [a, b, c] = arr1
+// console.log(a, b)
+
+// alert(Number("123"));
+// alert(Number("123z"));
+// alert(Number(true));
+// alert(Number(false));
+
+// let a = 5, b = 10
+// console.log(+a + +b);
+// let x = 8, y = 2
+// console.log(x / y)
+// let base = 3, exp = 4
+// console.log(base ** exp);
+// let s = 17, d = 5
+// console.log(s % d);
+// let count = 10
+// let counts = 10
+// console.log(++count);
+// console.log(--counts);
+
+// let str = 20, num = 5
+// console.log(+str + +5);
+// let a = 15, b = 20
+// console.log(a > b);
+// let x = 10, y = "10"
+// console.log(x == y);
+// console.log(x === y);
+// let num1 = 7, num2 = 13
+// console.log((num1 + num2) / 2);
+
+
